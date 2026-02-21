a5be976249f4c74b263762845d06c0e88626c038
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-02-21 22:39:55 +0300

    Refactor watchlist routes to include removeFromWatchlist and updateWatchlistItem functions

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import router from "./movieRoutes.js";
-import { addTowatchlist } from "../controllers/watchlist.contoller.js";
+import { addTowatchlist, removeFromWatchlist, updateWatchlistItem } from "../controllers/watchlist.contoller.js";
 import authMiddleware from "../middleware/auth.midleware.js";
 
 const routerAuth = express.Router();
 
 routerAuth.post("/", authMiddleware, addTowatchlist);
 
-routerAuth.delete("/:id", authMiddleware, async (req, res) => {})
-
+routerAuth.delete("/:id", authMiddleware, removeFromWatchlist);
+routerAuth.put("/:id", authMiddleware, updateWatchlistItem);
 
 export default routerAuth;
